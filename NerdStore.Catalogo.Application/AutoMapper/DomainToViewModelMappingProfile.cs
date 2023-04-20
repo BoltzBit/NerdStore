97eeefdd476f30f74c567aa4944aa3f7530a19ba
mateus.silva <mateus.silva@iatec.com>
2023-04-20 13:23:27 -0300

    Add application service

---
+++ NerdStore.Catalogo.Application/AutoMapper/DomainToViewModelMappingProfile.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using NerdStore.Catalogo.Application.ViewModels;
+using NerdStore.Catalogo.Domain;
+
+namespace NerdStore.Catalogo.Application.AutoMapper;
+
+public class DomainToViewModelMappingProfile : Profile
+{
+    public DomainToViewModelMappingProfile()
+    {
+        CreateMap<Produto, ProdutoViewModel>()
+            .ForMember(pv => pv.Altura,
+                output => output
+                    .MapFrom(p => p.Dimensoes.Altura))
+            .ForMember(pv => pv.Largura,
+                output => output
+                    .MapFrom(p => p.Dimensoes.Largura))
+            .ForMember(pv => pv.Profundidade,
+                output => output
+                    .MapFrom(p => p.Dimensoes.Profundidade));
+
+        CreateMap<Categoria, CategoriaViewModel>();
+    }
+}
